e2891b1961f04cac2d7c1e085f20935f99759d73
Gwylim Ashley <gwylim.a+launchpad@gmail.com>
2011-02-13 12:31:36 +0200

    -
    --HG--
    extra : convert_revision : gwylim.a%2Blaunchpad%40gmail.com-20110213103136-bu52eo4967l1vp15

--- umonya/apply/views.py
+++ umonya/apply/views.py
@@ -1,6 +1,25 @@
 # Create your views here.
 from django.http import HttpResponse
+from django import forms
+from django import template
 from umonya.apply.models import Student, Application
 
+class StudentApplyForm(forms.Form):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    school = forms.CharField(max_length=50)
+    alt_event = forms.BooleanField()
+    grade = forms.IntegerField()
+    email = forms.EmailField()
+    concat_no = forms.CharField(max_length=20)
+    alt_contact_no = forms.CharField(max_length=20, required=False)
+
 def apply(request):
-    return HttpResponse('')
+    if request.method == 'POST': # If the form has been submitted...
+        form = StudentApplyForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            pass
+    else:
+        form = StudentApplyForm() # An unbound form
+    t = template.loader.get_template('form.html')
+    return HttpResponse(t.render(Context({'form': form})))
