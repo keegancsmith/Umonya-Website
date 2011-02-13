b6d9c9f46abfffb6e9782254437459d2fe4bb68c
Kosie van der Merwe <kosie.vandermerwe@gmail.com>
2011-02-13 14:42:02 +0200

    update schema and models
    --HG--
    extra : convert_revision : kosie.vandermerwe%40gmail.com-20110213124202-90s91u93cq2y0r4p

--- umonya/apply/models.py
+++ umonya/apply/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Student(models.Model):
     student_id     = models.CharField(max_length=36, unique=True)
     first_name     = models.CharField(max_length=30, null=True, blank=True)
@@ -25,7 +24,7 @@ class Teacher(models.Model):
 class Application(models.Model):
     student_id         = models.ForeignKey(Teacher, related_name="applications")
     event_id           = models.ForeignKey(Event, related_name="applications")
-    creation_time      = models.DateTimeField()
+    creation_datetime  = models.DateTimeField()
     motivation         = models.TextField(null=True, blank=True)
     special_motivation = models.TextField(blank=True, null=True, blank=True)
     referral_source_id = models.ForeignKey(ReferralSource, related_name="applications")
@@ -38,10 +37,11 @@ class Application(models.Model):
     active_ind         = models.BooleanField(default=True)
 
 class Event(models.Model):
-    datetime   = models.DateTimeField()
-    location   = models.ForeignKey(Location, related_name="events")
-    url        = models.URLField(max_length=100)
-    active_ind = models.BooleanField(default=True)
+    start_datetime = models.DateTimeField()
+    end_datetime   = models.DateTimeField()
+    location_id    = models.ForeignKey(Location, related_name="events")
+    url            = models.URLField(max_length=100)
+    active_ind     = models.BooleanField(default=True)
 
 class Status(models.Model):
     description = models.CharField(max_length=100)
@@ -70,10 +70,10 @@ class Reviewer(models.Model):
     event_reviewer_id = models.ManyToManyField(Event, related_name="reviewers")
 
 class Review(models.Model):
-    application_id = models.ForeignKey(Application, related_name="reviews")
-    event_id       = models.ForeignKey(Event, related_name="reviews")
-    creation_time  = models.DateTimeField()
-    reviewer_id    = models.ForeignKey(Reviewer, related_name="reviews")
-    status_id      = models.ForeignKey(Status, null=True, blank=True)
-    comment        = models.TextField(null=True, blank=True)
-    rating         = models.IntegerField(null=True, blank=True)
+    application_id    = models.ForeignKey(Application, related_name="reviews")
+    event_id          = models.ForeignKey(Event, related_name="reviews")
+    creation_datetime = models.DateTimeField()
+    reviewer_id       = models.ForeignKey(Reviewer, related_name="reviews")
+    status_id         = models.ForeignKey(Status, null=True, blank=True)
+    comment           = models.TextField(null=True, blank=True)
+    rating            = models.IntegerField(null=True, blank=True)
