cc70d4b52121483c828b3024ae98330d23fe839f
Gwylim Ashley <ashgwy001@pc16.tsl.uct.ac.za>
2011-02-13 12:01:28 +0200

    removed forms
    --HG--
    extra : convert_revision : ashgwy001%40pc16.tsl.uct.ac.za-20110213100128-kaw0loh8fntjzp4y

--- umonya/apply/views.py
+++ umonya/apply/views.py
@@ -1,15 +1,6 @@
 # Create your views here.
-from django import forms
+from django.http import HttpResponse
 from umonya.apply.models import Student, Application
 
 def apply(request):
-    ApplyForm = forms.form_for_model(Application)
-
-    if request.method == 'POST':
-        form = ApplyForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = ApplyForm()
-
-    return render_to_response('form.html', { 'form' : form })
+    return HttpResponse('')
