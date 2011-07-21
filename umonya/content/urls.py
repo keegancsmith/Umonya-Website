23368e02651bb5e450bca63ecd22595c395a8fee
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2011-07-21 19:35:37 +0200

    Cleaned up so that it uses new features in Django 1.3
    --HG--
    rename : static/css/style.css => umonya/content/static/css/style.css
    rename : static/img/header-bg2.png => umonya/content/static/img/header-bg2.png
    rename : static/img/logos/google_logo.png => umonya/content/static/img/logos/google_logo.png
    rename : static/img/logos/python-logo.gif => umonya/content/static/img/logos/python-logo.gif
    rename : static/img/logos/python-logo.png => umonya/content/static/img/logos/python-logo.png
    rename : static/img/logos/sun_logo.jpg => umonya/content/static/img/logos/sun_logo.jpg
    rename : static/img/logos/sun_logo.png => umonya/content/static/img/logos/sun_logo.png
    rename : static/img/logos/uctlogo.png => umonya/content/static/img/logos/uctlogo.png
    rename : static/img/social/facebook.png => umonya/content/static/img/social/facebook.png
    rename : static/img/social/rss.png => umonya/content/static/img/social/rss.png
    rename : static/img/social/twitter.png => umonya/content/static/img/social/twitter.png
    rename : static/img/umonya-sized-white.png => umonya/content/static/img/umonya-sized-white.png
    rename : static/img/umonya-sized.png => umonya/content/static/img/umonya-sized.png
    rename : static/img/umonya-small.png => umonya/content/static/img/umonya-small.png
    rename : templates/about-us.html => umonya/content/templates/about-us.html
    rename : templates/contact-us.html => umonya/content/templates/contact-us.html
    rename : templates/events.html => umonya/content/templates/events.html
    rename : templates/get-involved.html => umonya/content/templates/get-involved.html
    rename : templates/home.html => umonya/content/templates/home.html
    rename : templates/media.html => umonya/content/templates/media.html
    rename : templates/resources.html => umonya/content/templates/resources.html
    rename : templates/sponsors.html => umonya/content/templates/sponsors.html
    rename : templates/apply.html => umonya/templates/apply.html
    rename : templates/skeleton.html => umonya/templates/skeleton.html
    extra : convert_revision : keegan.csmith%40gmail.com-20110721173537-xk2f5m8vink10u5j

---
+++ umonya/content/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import include, patterns
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns(
+    '',
+
+    # Direct to template
+    (r'^$'            , direct_to_template, {'template':'home.html'}),
+    (r'^contact-us'   , direct_to_template, {'template':'contact-us.html'}),
+    (r'^get-involved' , direct_to_template, {'template':'get-involved.html'}),
+    (r'^media'        , direct_to_template, {'template':'media.html'}),
+    (r'^resources'    , direct_to_template, {'template':'resources.html'}),
+    (r'^sponsors'     , direct_to_template, {'template':'sponsors.html'}),
+    (r'^about-us'     , direct_to_template, {'template':'about-us.html'}),
+)
--- umonya/urls.py
+++ umonya/urls.py
@@ -1,35 +1,17 @@
-from umonya.apply.views import *
-
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
-from django.views.generic.simple import direct_to_template
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
-
 
 urlpatterns = patterns(
     '',
 
-    # Static files
-    (r'^(?P<path>img/.*)$', 'django.views.static.serve',
-     {'document_root': settings.STATIC_DOC_ROOT}),
-    (r'^(?P<path>css/.*)$', 'django.views.static.serve',
-     {'document_root': settings.STATIC_DOC_ROOT}),
-    (r'^(?P<path>js/.*)$', 'django.views.static.serve',
-     {'document_root': settings.STATIC_DOC_ROOT}),
-
-    # Direct to template
-    (r'^$'            , direct_to_template, {'template':'home.html'}),
-    (r'^contact-us'   , direct_to_template, {'template':'contact-us.html'}),
-    (r'^get-involved' , direct_to_template, {'template':'get-involved.html'}),
-    (r'^media'        , direct_to_template, {'template':'media.html'}),
-    (r'^resources'    , direct_to_template, {'template':'resources.html'}),
-    (r'^sponsors'     , direct_to_template, {'template':'sponsors.html'}),
-    (r'^about-us'     , direct_to_template, {'template':'about-us.html'}),
-
+    (r'', include('umonya.content.urls')),
     (r'^events/', include('umonya.apply.urls')),
 
     (r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
