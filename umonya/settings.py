a8e936570ce5f06c37383b201f125e0dffd7c42f
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2011-07-21 20:07:46 +0200

    Cleaned up settings.py
    --HG--
    extra : convert_revision : keegan.csmith%40gmail.com-20110721180746-03zq4q7oamoni88v

--- umonya/settings.py
+++ umonya/settings.py
@@ -1,18 +1,14 @@
 # Django settings for umonya project.
+# For settings relevant to deployment, search the document for the word DEPLOY
 
 import os.path
-
 project_dir = os.path.abspath(os.path.dirname(__file__))
 
+# DEPLOY set to False
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    ('Umonya Admins', 'admins@umonya.co.za'),
-)
-
-MANAGERS = ADMINS
-
+# DEPLOY set this to the local postgresql or mysql db
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -24,77 +20,47 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# Locale
 TIME_ZONE = 'Africa/Johannesburg'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-za'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
+# User uploaded files variables. Currently not used
 MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
+# Static file locations. DEPLOY The first two settings should be modified when
+# deploying static files to a seperate URL.
 STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_DIRS = ()
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
+# DEPLOY Change this value for actual deployed sites
 SECRET_KEY = 'mn=y5m0vfbcq1m6(84pkoove40&rel_^yzs$te4oq1c8ug^zsr'
 
-# List of callables that know how to import templates from various sources.
+# Misc
+SITE_ID = 1
+ROOT_URLCONF = 'umonya.urls'
+ADMINS = (
+    ('Umonya Admins', 'admins@umonya.co.za'),
+)
+MANAGERS = ADMINS
+
+# Template Settings
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
+TEMPLATE_DIRS = (
+    os.path.join(project_dir, 'templates'),
+)
+
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -104,14 +70,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'umonya.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(project_dir, 'templates'),
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -124,11 +82,8 @@ INSTALLED_APPS = (
     'umonya.apply',
     'umonya.content',
 )
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
